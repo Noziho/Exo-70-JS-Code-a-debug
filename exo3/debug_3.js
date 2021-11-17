743b10da3753d9ef57081aeb2ba849bc8be4334e
Noah2211 <noah.decroix3@gmail.com>
2021-11-17 19:47:24 +0100

    exercice 2 done

--- exo3/debug_3.js
+++ exo3/debug_3.js
@@ -1,25 +1,31 @@
 let texte = document.getElementById('main').innerHTML = "test";
 
-let boucle;
-while(var boucle<5) {
-    boucle =+ 5;
+let boucle = 0;
+
+while (boucle < 5) {
+
+    boucle += 5;
+    document.getElementById('main').innerHTML = "ttttttttttttttttt";
+}
+
+let a = 1;
+
+if (a === 1) {
+
+    alert('connue');
+} else {
+
+    alert("inconnue");
 }
 
 
-document.getElementById('divInconnuAuBataillon').innerHTML = "ttttttttttttttttt";
-
-if(variableIconnueAuBataillon == 1) {
-alert('connue');
-}
-else {
-    alert(inconnue);
-}
+alert("Penser à ne pas oublier...");
+alert("certaines choses");
 
 
-alert("Penser à ne pas oublier...);
 
 
-alert "certaines choses";
+
 
 
 
