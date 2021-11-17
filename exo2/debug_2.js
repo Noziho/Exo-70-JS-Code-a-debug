e47b9f7f6fe63f1b23ace114b9bd6aa7931c967e
Noah2211 <noah.decroix3@gmail.com>
2021-11-17 16:50:45 +0100

    exercice 2 done

--- exo2/debug_2.js
+++ exo2/debug_2.js
@@ -1,27 +1,24 @@
-let main = document.getElementById(main);
+let main = document.getElementById('main');
 
 main.id = "main2";
-
 main.style.display = "hidden";
+main.innerText = "Mon nouveau texte";
 
-document.getElementById('main') = "Mon nouveau texte !";
 
-let html = main.innerHTLM;
-
+let html = main.innerText;
 alert(html);
 
-if(html) {
-    console.log(html);
-}
-else {
-    let test = document.getElementParId('test');
-    if(test) {
-        alert(test);
-    }
+let a = 1;
+if (a === 1) {
+
+    console.log("ceci  " + html);
 }
 
+else if (a === 0) {
+    let test = document.getElementById('test');
+    alert(test);
+}
 
-let main = getElementById('main');
-main[style][display] = "block";
+main.style.display = "block";
 
 
